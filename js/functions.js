55206bf7d5b8fc28f63b6718be42bc43be81d5a1
Jeannette Rojas <janerrodriguez92@gmail.com>
2017-10-25 13:49:32 -0600

    cmbi

--- js/functions.js
+++ js/functions.js
@@ -28,7 +28,7 @@ function obtenerXHR()
     return req; 
 }; 
 
-function agregarEquipo()
+function agregarEquipos()
 {
           
   var country = document.getElementById("country").value;
@@ -64,6 +64,50 @@ function agregarEquipo()
 
   document.getElementById("resultado").innerHTML=" \
   Equipo Agregado\
+  <br> idGrupo: "+country+" \
+  <br> idTutoria: "+confederation+" \
+  <br> cupo: "+points+" \
+  <br> anno: "+flag;
+
+  document.getElementById("form1").reset();
+}
+
+function actualizarEquipos()
+{
+          
+  var country = document.getElementById("country").value;
+  var confederation = document.getElementById("confederation").value;
+  var points = document.getElementById("points").value;
+  var flag = document.getElementById("flag").value;
+ 
+
+//IMPLEMENTACION DEL PHP
+  var peticion = obtenerXHR(); 
+  url="../php/actualizarEquipos.php?tipo=insertar";
+  url+="&country="+country;
+  url+="&confederation="+confederation;
+  url+="&points="+points;
+  url+="&flag="+flag;
+
+  peticion.open("GET", url , true); 
+  peticion.onreadystatechange=function ()
+    {
+      if (peticion.readyState===4)
+      {
+        if (peticion.status===200)
+        {
+          if (peticion.responseText===1)
+          {
+            alert ("si se pudo insertar");
+          }
+        }
+      }
+    }
+    ;
+  peticion.send(null);
+
+  document.getElementById("resultado").innerHTML=" \
+  Equipo actualizado\
   <br> idGrupo: "+country+" \
   <br> idTutoria: "+confederation+" \
   <br> cupo: "+points+" \
